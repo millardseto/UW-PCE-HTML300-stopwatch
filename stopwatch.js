29daa61ec91660179491aa8bb988c92559abe1c6
Millard Seto <millardseto@mac.com>
2017-06-19 20:58:16 -0700

    Implement start and stop

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,19 @@
+// button controls
+const start = document.querySelector('button.start');
+const stop = document.querySelector('button.stop');
+
+// DOM elements that need to update
+const lapList = document.querySelector('#lapList');
+const stopwatchTime = document.querySelector('#stopwatchTime');
+
+// constants that shouldn't ever change
+const laps = [];
+const intervalRate = 10; // update the stopwatch every xxx milliseconds
+
+// values that will change pretty often
+let intervalId = null;
+let rawTime = 0;
+
 // turns the time into a human readable format
 function formatTime (raw) {
   let seconds = Math.floor(raw / 1000)
@@ -14,6 +30,42 @@ function zeroPad (value) {
   return `${pad}${value}`
 }
 
+start.addEventListener('click', stopwatchStart);
+stop.addEventListener('click', stopwatchStop);
+
+function stopwatchStart(){
+  event.preventDefault();
+  console.log('started');
+
+  intervalId = setInterval(stopwatchUpdate, intervalRate);
+}
+
+function stopwatchStop(){
+  event.preventDefault();
+  console.log('stopped');
+
+  clearInterval(intervalId);
+}
+
+
+
+function stopwatchUpdate(){
+  rawTime += intervalRate;
+  stopwatchTime.innerHTML = rawTime;
+}
+
+function stopwatchLap(){
+  event.preventDefault();
+  console.log('lapped');
+}
+
+function stopwatchReset(){
+  event.preventDefault();
+  console.log('reset');
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
+
+
 })
